1bf1201af495d50fa9298d1880ee2864ca994f7f
Padraig Gleeson <p.gleeson@gmail.com>
2014-05-01 11:55:11 +0100

    Further tweaking; adding 4 cell net with exc/inh syns

--- CElegans/pythonScripts/c302/c302_A.py
+++ CElegans/pythonScripts/c302/c302_A.py
@@ -4,7 +4,7 @@ import parameters_A as params
 
 if __name__ == '__main__':
     
-    cells_to_stimulate = ["ADAL", "ADAR"]
+    cells_to_stimulate = ["M1","M2L","M2R","M3L","M3R","M4","M5","I1L","I1R","I2L","I2R","I3","I4","I5","I6","MI","NSML","NSMR","MCL","MCR"]
     cells_to_plot      = ["ADAL", "ADAR", "PVDR", "BDUR"]
     
     generate("c302_A", params, cells_to_plot=cells_to_plot, cells_to_stimulate=cells_to_stimulate, duration=500, dt=0.1, vmin=-72, vmax=-48)
--- CElegans/pythonScripts/c302/parameters_A.py
+++ CElegans/pythonScripts/c302/parameters_A.py
@@ -12,18 +12,18 @@ iaf_C =                 BioParameter("iaf_C", "0.2nF", "BlindGuess", "0.1")
 iaf_conductance =       BioParameter("iaf_conductance", "0.01uS", "BlindGuess", "0.1")
 
 
-chem_exc_syn_gbase =       BioParameter("chem_exc_syn_gbase", "0.06nS", "BlindGuess", "0.1")
+chem_exc_syn_gbase =       BioParameter("chem_exc_syn_gbase", "0.5nS", "BlindGuess", "0.1")
 chem_exc_syn_erev =        BioParameter("chem_exc_syn_erev", "0mV", "BlindGuess", "0.1")
 chem_exc_syn_rise =        BioParameter("chem_exc_syn_rise", "3ms", "BlindGuess", "0.1")
 chem_exc_syn_decay =       BioParameter("chem_exc_syn_decay", "10ms", "BlindGuess", "0.1")
 
-chem_inh_syn_gbase =       BioParameter("chem_inh_syn_gbase", "0.06nS", "BlindGuess", "0.1")
+chem_inh_syn_gbase =       BioParameter("chem_inh_syn_gbase", "0.5nS", "BlindGuess", "0.1")
 chem_inh_syn_erev =        BioParameter("chem_inh_syn_erev", "-80mV", "BlindGuess", "0.1")
 chem_inh_syn_rise =        BioParameter("chem_inh_syn_rise", "3ms", "BlindGuess", "0.1")
 chem_inh_syn_decay =       BioParameter("chem_inh_syn_decay", "10ms", "BlindGuess", "0.1")
 
 
-unphysiological_offset_current = BioParameter("unphysiological_offset_current", "0.25nA", "KnownError", "0")
+unphysiological_offset_current = BioParameter("unphysiological_offset_current", "0.21nA", "KnownError", "0")
 unphysiological_offset_current_dur = BioParameter("unphysiological_offset_current_dur", "200ms", "KnownError", "0")
 
 
