6999a335ca829210652d92c0415a60453e86d021
Mark Watts <markw@cs.utexas.edu>
2014-05-28 11:51:34 -0500

    Updated script to send neuroml to database

--- CElegans/pythonScripts/nml_to_graph.py
+++ CElegans/pythonScripts/nml_to_graph.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
+from glob import glob
 import rdflib as R
+import httplib as H
 import numbers
 from numbers import Number
 import neuroml.loaders as loaders
@@ -10,38 +12,26 @@ from lxml import etree as ET
 
 ns = R.Namespace("http://www.neuroml.org/schema/neuroml2/")
 ns1 = R.Namespace("http://www.markw.com/coolURL#")
-def k():
-    doc = loaders.NeuroMLLoader.load("../generatedNeuroML2/ADAL.nml")
-    c = doc.cells[0]
-    for p in z(c.morphology):
-        yield p
-def k_xml():
-    c = lxml.parse("../generatedNeuroML2/ADAL.nml")
-    for p in z(c):
-        yield p
+namespaces = {"ns" :"http://www.neuroml.org/schema/neuroml2/"}
+def triples_from_morphology(f):
+    c = ET.parse(f).getroot()
+    c = c.xpath("*")[1]
+    for i in z(c,R.BNode()):
+        yield i
 
-def z(m):
-    mNode = R.BNode()
+def z(m, node):
     for k in m:
-        d = getattr(m, k)
-        if isinstance(d,list):
-            n = R.BNode()
-            yield (mNode, ns[k], n)
-            for p in d:
-                if isinstance(p, basestring):
-                    yield (n, ns1["hasMember"], p)
-                else:
-                    for i in z(p):
-                        yield(i)
-        else:
-            if is_valid_datatype(d):
-                yield (mNode, ns[k], R.Literal(d))
-            elif d != None:
-                n = R.BNode()
-                yield (n, R.RDFS['label'], R.Literal(k))
-                for i in z(d):
-                    yield(i)
-                print (k,d)
+        if isinstance(k,type(m)):
+            c = R.BNode()
+            n = ET.QName(k.tag).localname
+            yield (node, ns[n], c)
+            for i in z(k,c):
+                yield i
+    t = m.text
+    if t != None and len(t.strip()) > 0:
+        yield (node, ns["text"], R.Literal(m.text))
+    for (k,v) in m.items():
+        yield (node, ns[k], R.Literal(v))
 
 
 def is_valid_datatype(v):
@@ -51,10 +41,21 @@ def is_valid_datatype(v):
             return True
     return False
 
+def put_in_sesame(graph):
+    s = graph.serialize(format="n3")
+    con = H.HTTPConnection("107.170.133.175:8080")
+    con.request("PUT", "/openrdf-sesame/repositories/test/statements", s, {"Content-Type": "application/x-turtle;charset=UTF-8"})
+    r = con.getresponse()
+    print "sesame response is %d " % r.status
 
 if __name__ == "__main__":
     graph = R.Graph()
-    for t in k():
-        graph.add(t)
-    graph.serialize("result.n3", "n3")
-
+    for x in iglob("../generatedNeuroML2/*.nml"):
+        try:
+            for p in triples_from_morphology(x):
+                graph.add(p)
+        except Exception, e:
+            print 'failed on file %s' % x
+            print 'error : ' + str(e)
+            pass
+    put_in_sesame(graph)
