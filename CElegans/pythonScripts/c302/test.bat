ca9e37e5900f9ade7ddd0ca2cbcc009b8f288ef7
Padraig Gleeson <p.gleeson@gmail.com>
2014-09-02 14:37:47 +0100

    Adding initial test script for Windows...

---
+++ CElegans/pythonScripts/c302/test.bat
@@ -0,0 +1,39 @@
+REM   (Re)generate NeuroML 2 & LEMS files from the python scripts
+
+python c302_A.py
+python c302_A_Pharyngeal.py
+python c302_A_Syns.py
+
+
+REM Validate generated NeuroML 2
+
+jnml -validate c302_A.nml
+jnml -validate c302_A_Pharyngeal.nml
+jnml -validate c302_A_Syns.nml
+
+goto comment 
+
+REM Try running these in jNeuroML with no GUI
+
+REM jnml LEMS_c302_A.xml -nogui    #  Takes 2 mins to run!
+
+jnml LEMS_c302_A_Pharyngeal.xml -nogui
+jnml LEMS_c302_A_Syns.xml -nogui
+
+ 
+REM Try regenerating using command line options
+
+python c302.py c302_A_Syns parameters_A -cells ["ADAL","AIBL","RIVR","RMEV"] -cellstostimulate ["ADAL","RIVR"] -duration 500 -dt 0.1 -vmin -72 -vmax -48
+
+python c302.py c302_A_Weights parameters_A -cells ["ADAL","AIBL","I1L","I3","DB5","PVCR"] -cellstostimulate ["ADAL","I1L","PVCR"] -connnumberoverride=["I1L-I3":2.5] -connnumberscaling=["PVCR-DB5":5] -duration 500 -dt 0.1 -vmin -72 -vmax -48
+
+
+REM Try converting it to NEURON
+
+jnml LEMS_c302_A.xml -neuron
+jnml LEMS_c302_A_Pharyngeal.xml -neuron
+jnml LEMS_c302_A_Syns.xml -neuron
+
+nrnivmodl
+
+:comment
