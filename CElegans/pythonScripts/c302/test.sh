a3729ef22fd2d361c611ffb02137bb3fd13f7eb1
Padraig Gleeson <p.gleeson@gmail.com>
2015-02-03 19:47:16 +0000

    Updating unittests for latest restructuring of c302
    More tests

--- CElegans/pythonScripts/c302/test.sh
+++ CElegans/pythonScripts/c302/test.sh
@@ -2,6 +2,7 @@ set -e
 
 ##   (Re)generate NeuroML 2 & LEMS files from the python scripts
 
+python c302_IClamp.py A
 python c302_Syns.py A
 python c302_Social.py A
 python c302_Pharyngeal.py A
@@ -9,12 +10,15 @@ python c302_Full.py A
 python c302_Muscles.py A
 
 
+python c302_IClamp.py B
 python c302_Syns.py B
 python c302_Social.py B
 python c302_Pharyngeal.py B
 python c302_Full.py B
 python c302_Muscles.py B
 
+
+python c302_IClamp.py B
 python c302_Syns.py C
 python c302_Social.py C
 python c302_Pharyngeal.py C
@@ -54,7 +58,7 @@ jnml LEMS_c302_B_Social.xml -nogui
 
 ## Try regenerating using command line options
 
-python c302.py c302_A_Syns parameters_A -cells ["ADAL","AIBL","RIVR","RMEV"] -cellstostimulate ["ADAL","RIVR"] -duration 500 -dt 0.1 -vmin -72 -vmax -48
+python c302.py c302_A_Syns2 parameters_A -cells ["ADAL","AIBL","RIVR","RMEV"] -cellstostimulate ["ADAL","RIVR"] -duration 500 -dt 0.1 -vmin -72 -vmax -48
 
 python c302.py c302_A_Weights parameters_A -cells ["ADAL","AIBL","I1L","I3","DB5","PVCR"] -cellstostimulate ["ADAL","I1L","PVCR"] -connnumberoverride=["I1L-I3":2.5] -connnumberscaling=["PVCR-DB5":5] -duration 500 -dt 0.1 -vmin -72 -vmax -48
 
@@ -62,6 +66,6 @@ python c302.py c302_A_Weights parameters_A -cells ["ADAL","AIBL","I1L","I3","DB5
 ## Try converting it to NEURON
 
 jnml LEMS_c302_A_Full.xml -neuron
-jnml LEMS_c302_A_Pharyngeal.xml -neuron
-jnml LEMS_c302_A_Syns.xml -neuron
+jnml LEMS_c302_B_Pharyngeal.xml -neuron
+jnml LEMS_c302_C_Syns.xml -neuron
 nrnivmodl
